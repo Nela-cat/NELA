03fd2903ff9cb6be4f6aae3d23ee6ec56e082e43
Nela-cat <74066037+Nela-cat@users.noreply.github.com>
2021-02-12 18:49:29 +0200

    Add files via upload

---
+++ les1-19/les29/main.js
@@ -0,0 +1,62 @@
+let v = prompt("Добрый вечер,готовы ответить на наши вопросы?")
+alert("отлично")
+
+let x = prompt("Как вас зовут?")
+alert("  Добрый вечер  " + x)
+
+let y = prompt("Сколько вам лет ?")
+alert("отлично    " + y)
+
+let p = prompt(" Хотите иметь стабильный заработок ?")
+alert(" отлично " + p)
+
+
+let z = prompt(" Чем планируете занимвться в нашей компании ?")
+alert(" отлично " + z)
+
+
+let w = prompt(" Кем вы видите себя чере пять лет?")
+alert(" Хорошо  " + w)
+
+
+let r = prompt(" Назовите свои лучшие качества ?")
+alert(" Приемлемо  " + r)
+
+
+let t = prompt(" Какие ваши слабые стороны ?")
+alert(" Приемлемо  " + t)
+
+let n = prompt(" Какая заработная плата вас устроит ?")
+alert(" Хорошо  " + n)
+
+let m = prompt(" Что думаете о нас ?")
+alert(" Хорошо  " + m)
+
+
+let s = prompt(" Что думаете о себе ?")
+alert(" Хорошо  " + s)
+
+
+let g = prompt(" Почему вы хотите работать у нас ?")
+alert("Отлично  " + g)
+
+
+
+
+
+
+
+
+console.log(v)
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(p)
+console.log(z)
+console.log(w)
+console.log(r)
+console.log(t)
+console.log(n)
+console.log(m)
+console.log(s)
+console.log(g)
