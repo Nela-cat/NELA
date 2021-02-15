4142f8bc02a751f308ca93736dbba67167193a8f
Nela-cat <74066037+Nela-cat@users.noreply.github.com>
2021-02-15 12:49:25 +0200

    Add files via upload

---
+++ les1-19/les30/main.js
@@ -0,0 +1,49 @@
+let x = 10
+let b = 12
+let j = "comp"
+
+// c = x - b
+// v = x + b
+// m = x * b
+// f = x ** b
+// j = x / b
+// q = x ^ b
+// w = x % b
+// x = x + 5
+// x += 5
+// b /= 7
+
+
+// console.log(b)
+// console.log(x)
+// console.log(x)
+// console.log(c)
+// console.log(v)
+// console.log(m)
+// console.log(f)
+// console.log(j)
+// console.log(q)
+// console.log(w)
+
+// x++
+// console.log(x)
+
+// b++
+// console.log(b)
+
+
+console.log(x == b)
+console.log(x != b)
+console.log(x < b)
+console.log(x > b)
+console.log(x => b)
+console.log(x <= b)
+console.log(b => x)
+console.log(b <= x)
+
+
+console.log(j < b)
+console.log(x > j)
+console.log(j <= b)
+console.log(b => j)
+console.log(b <= j)
