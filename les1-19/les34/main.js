04741b68ec3ffa27031787721073fad837c88b0b
Nela-cat <74066037+Nela-cat@users.noreply.github.com>
2021-02-22 19:49:57 +0200

    Add files via upload

---
+++ les1-19/les34/main.js
@@ -0,0 +1,91 @@
+// let x = prompt("Ваш возраст?")
+// if (x < 18) {
+
+//     alert("старш. к. ")
+// } else if (x < 6) {
+//     alert("младш. к.")
+// } else {
+//     alert("не шк")
+// }
+
+
+
+
+
+
+
+// let a = Number(prompt("сравни a"))
+// let b = Number(prompt("сравни b "))
+// if (a < b) {
+
+//     alert(" a < b")
+// } else if (a > b) {
+//     alert("a > b")
+// } else if (x == b) {
+//     alert("x == b")
+// } else if (x != b) {
+//     alert("x != b")
+// } else {
+//     alert("нет")
+// }
+
+// let c = Number(prompt("сравни c "))
+// let d = Number(prompt("сравни d "))
+// if (c < d) {
+
+//     alert(" c < d")
+// } else if (c > d) {
+//     alert("c > d")
+// } else if (c == d) {
+//     alert("c == d")
+// } else if (c != d) {
+//     alert("c != d")
+// } else {
+//     alert("нет")
+// }
+
+
+
+
+// let a = prompt("треугольник равнобед")
+
+// if (a == "да") {
+//     alert("две стор")
+// } else {
+//     alert("разнос")
+// }
+
+
+
+// let c = prompt("Кто ты ")
+
+// if (c == "поль") {
+
+//     alert(" регистрируйтесь")
+
+// } else if (c == "админ") {
+//     let t = prompt("введите пароль")
+//     console.log(t)
+
+// } else if (c == "директор") {
+//     alert("добро пожаловать")
+// } else {
+//     alert("нет")
+// }
+
+let c = prompt("Вы решили задачу? ")
+
+if (c == "да") {
+
+    alert(" иди домой")
+
+} else if (c == "нет") {
+    t = prompt("До когда остаемся?")
+    alert(" ну все оставайся до  " + t)
+
+
+} else if (c == "за 5 минут") {
+    alert("скоро будет шоколад")
+} else {
+    alert("нет")
+}
