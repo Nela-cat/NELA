69a0bc5da66cfc86c8e71b79c99cadeb0f65ad84
Nela-cat <74066037+Nela-cat@users.noreply.github.com>
2021-03-19 14:29:23 +0200

    Add files via upload

---
+++ les1-19/les36/main.js
@@ -0,0 +1,42 @@
+// let x = prompt("введите кисло")
+// if (x == 10) {
+//     alert("верно")
+// } else {
+//     alert("не верно")
+// }
+
+
+
+// let x = prompt("введите число")
+// if (x >= "0" && x <= "14") {
+//     alert("в первой")
+// } else if (x >= "15" && x <= "30") {
+//     alert("в четвертой")
+// } else if (x >= "31" && x <= "45") {
+//     alert("в третей")
+// } else {
+//     alert("втарая")
+// }
+
+
+
+
+
+// let w = prompt(" введите яз")
+// let r = arr = ["пн", "вт", "ср", "чт", "пят", "сб", "вс"]
+// let en = arr = ["mn", "ts", "р", "т", "т", "б", "в"]
+// if (w == "ru") {
+//     alert(r)
+// } else if (w == "en") {
+//     alert(en)
+// }
+
+
+
+// let a = Number(prompt("введите число a"))
+// let b = Number(prompt("введите число b"))
+// if (a <= 1 && b >= 3) {
+//     alert(a + b)
+// } else {
+//     alert(a - b)
+// }
---
+++ les1-19/les37/main.js
@@ -0,0 +1,87 @@
+// let x = prompt("введите кисло")
+// if (x == 10) {
+//     alert("верно")
+// } else {
+//     alert("не верно")
+// }
+
+
+
+// let x = prompt("введите число")
+// if (x >= "0" && x <= "14") {
+//     alert("в первой")
+// } else if (x >= "15" && x <= "30") {
+//     alert("в четвертой")
+// } else if (x >= "31" && x <= "45") {
+//     alert("в третей")
+// } else {
+//     alert("втарая")
+// }
+
+
+
+
+
+// let w = prompt(" введите яз")
+// let r = arr = ["пн", "вт", "ср", "чт", "пят", "сб", "вс"]
+// let en = arr = ["mn", "ts", "р", "т", "т", "б", "в"]
+// if (w == "ru") {
+//     alert(r)
+// } else if (w == "en") {
+//     alert(en)
+// }
+
+
+
+// let a = Number(prompt("введите число a"))
+// let b = Number(prompt("введите число b"))
+// if (a <= 1 && b >= 3) {
+//     alert(a + b)
+// } else {
+//     alert(a - b)
+// }
+
+
+
+
+
+
+
+
+
+// // номер 11
+// let a = Number(prompt("введите число"))
+// if (a > 0 && a < 5) {
+//     alert(" Верно!")
+// } else {
+//     alert("неверно")
+// }
+// console.log(a)
+
+
+
+
+
+
+// номер 12
+// let a = Number(prompt("введите число"))
+// if (a == 0 || a == 2) {
+//     alert(" a + 7  =   " + (a + 7))
+// } else {
+//     alert("a / 10  =   " + (a / 10))
+// }
+// console.log(a)
+// console.log(a + 7)
+// console.log(a / 10)
+
+
+
+
+// номер 14
+let a = Number(prompt("введите число a "))
+let b = Number(prompt("введите число b"))
+if (a > 2 && a < 11 || b >= 6 && b < 14) {
+    alert(" verno")
+} else {
+    alert(" neverno")
+}
